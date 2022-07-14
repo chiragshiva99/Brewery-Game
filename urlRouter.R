dc9430258792527be77ca77db37bbbc407d5c756
Chirag Shivakumar <86071790+chiragshiva99@users.noreply.github.com>
2022-07-14 18:03:17 +0800

    Update urlRouter.R

--- urlRouter.R
+++ urlRouter.R
@@ -3,10 +3,10 @@ library(shiny.router)
 #root_page <- div(h2("Root page"))
 #other_page <- div(h3("Other page"))
 
-router <- make_router(
-  route("/", root_page),
+#router <- make_router(
+#  route("/", root_page),
 #  route("other", other_page)
-)
+#)
 
 
 

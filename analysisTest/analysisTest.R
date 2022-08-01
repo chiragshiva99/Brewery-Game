856e15e2e5f1f52c016c4c6823b6690a5af9dfec
Chirag Shivakumar <86071790+chiragshiva99@users.noreply.github.com>
2022-08-01 21:24:53 +0800

    Update analysisTest.R

--- analysisTest/analysisTest.R
+++ analysisTest/analysisTest.R
@@ -15,9 +15,7 @@ getFromTrackTable <- function(table, userID, gameID) {
   return(result)
 }
 
-tank <- getFromTrackTable("demandTrack", 1, 14)
 tank <- getFromTrackTable("tankTrack", 1, 18)
-
 tank
 
 ggplot(tank, aes(gameDay, tankID)) + 
